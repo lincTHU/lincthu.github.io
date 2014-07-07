63a1b6c7e3cab3cfdf8510b5b8fc3b4cf3b7e889
lincTHU <lincong12@126.com>
2014-07-07 23:43:12 +0800

    hw3作业

---
+++ hw3_learn/提交作业.js
@@ -0,0 +1,6 @@
+﻿button = document.getElementById("submit");
+button.style.marginBottom = 0;
+s = document.getElementsByClassName("section");
+s[2].parentNode.appendChild(s[2]);
+s[2].parentNode.appendChild(s[2]);
+s[2].parentNode.appendChild(button.parentNode);
---
+++ hw3_learn/课程作业.js
@@ -0,0 +1,17 @@
+﻿tb = document.getElementById("homeworklist").firstElementChild.children;
+for(i = 0; i < tb.length; i++)
+{
+	item = tb[i].children;
+	if (item[2].firstElementChild.innerText != '已改批改') 
+	{
+		item[8].firstElementChild.setAttribute('style', 'color:#969696');
+		item[8].firstElementChild.removeAttribute("href");
+	}
+	else (item[2].firstElementChild.innerText = '批改完毕')
+	tb[i].insertBefore(item[2], item[6]);
+	tb[i].appendChild(item[6]);
+}
+tb = document.getElementsByClassName("hw-table-title")[0].firstElementChild.firstElementChild.firstElementChild;
+state = tb.children;
+tb.insertBefore(state[2], state[6]);
+tb.appendChild(state[6]);
