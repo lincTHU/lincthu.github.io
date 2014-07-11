8b1670d2b91334b3311a142730c003616e548a8b
lincTHU <lincong12@126.com>
2014-07-12 01:55:12 +0800

    增加了图片

--- hw4_picture_html/js/comment_content.js
+++ hw4_picture_html/js/comment_content.js
@@ -31,33 +31,33 @@
             "date": "2014/7/11/5:58:60"
         },
         {
-            "username": "皮卡丘",
-            "face": "1.jpg",
-            "content": "皮卡丘~",
+            "username": "迷之馆长",
+            "face": "6.jpg",
+            "content": "楼上老衲与你有缘，找个时间见个面怎么样呀~",
             "date": "2014/7/11/5:58:60"
         },
         {
-            "username": "皮卡丘",
-            "face": "1.jpg",
-            "content": "皮卡丘~",
+            "username": "YAO明",
+            "face": "7.jpg",
+            "content": "楼上和楼上上，不如我们今日义结金兰",
             "date": "2014/7/11/5:58:60"
         },
         {
-            "username": "路飞",
-            "face": "2.jpg",
-            "content": "开总库哇，我累哇，拿路！",
+            "username": "樱木",
+            "face": "8.jpg",
+            "content": "教练我想学WEB前端设计TAT",
             "date": "2014/7/11/3:58:60"
         },
         {
-            "username": "皮卡丘",
-            "face": "1.jpg",
-            "content": "皮卡丘~",
+            "username": "教♂练",
+            "face": "9.jpg",
+            "content": "你TM什么都想学！",
             "date": "2014/7/11/5:58:60"
         },
         {
-            "username": "路飞",
-            "face": "2.jpg",
-            "content": "开总库哇，我累哇，拿路！",
+            "username": "拿路透",
+            "face": "10.jpg",
+            "content": "楼上上的小樱等等我!!",
             "date": "2014/7/11/3:58:60"
         },
         {
