16c86341001b6e57dba68c12e2bdc6514c977b27
DESKTOP-E79G8H7\CongLin-PC <lin.co@husky.neu.edu>
2018-01-19 20:00:29 -0500

    super savage bahamut guide

---
+++ bahamut/js/hover-global.js
@@ -0,0 +1,31 @@
+
+$(".status-area").mouseenter(
+	function(){
+/*	var currentMousePos = { x: -1, y: -1 };
+    $(document).mousemove(function(event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+    })*/
+
+    var id = $(this).attr("name");
+    var status = statusDB[id];
+    //load status pic
+    $(".hover-frame").find("img").attr("src", status.src);
+    $(".hover-header-name").text(status.name);
+    $(".hover-content-discription-info").text(status.info);
+    if (status.duration != undefined){
+   		$(".hover-content-discription-duration").html("<a style=\"color:#0AA30E\">持续时间：</a>"+ status.duration + "秒");
+   	}
+   	else $(".hover-content-discription-duration").text("");
+
+    //load status location
+    var statusOffset = $(this).offset();
+    statusOffset.top += 20;
+    statusOffset.left += 20;
+	$(".hover-frame").css(statusOffset);
+	$(".hover-frame").show();
+	}
+).mouseleave(function(){
+		$(".hover-frame").hide();
+	}
+);
---
+++ bahamut/js/status-database.js
@@ -0,0 +1,24 @@
+var statusDB = {
+	"Burns": {
+		"name": "火伤",   
+		"src": "img\\status\\Burns.png", 
+		"info": "火属性持续伤害，体力逐渐流失(即死)",
+		"duration": 30
+	},
+    "Neurolink": {
+		"name": "拘束",   
+		"src": "img\\status\\Neurolink.png", 
+		"info": "受到拘束，造成的伤害大幅度减少(90%)，同时大幅度减少来自特定技能的伤害。"
+	},
+    "SlashingResistanceDownII": {
+		"name": "斩击耐性大幅度下降",   
+		"src": "img\\status\\SlashingResistanceDownII.png", 
+		"info": "受到拘束，造成的伤害大幅度减少(90%)，同时大幅度减少来自特定技能的伤害。",
+		"duration": 35
+	},
+    "ManaHypersensitivity": {
+		"name": "魔力过敏症",   
+		"src": "img\\status\\ManaHypersensitivity.png", 
+		"info": "提高受到来自\"魔力炼成\"的伤害。"
+	}
+}
